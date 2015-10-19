d8db710db12336d6bd7925281174b89249fe9fcb
Mikhail <mvaluyskiy@weezlabs.com>
2015-10-19 15:23:53 +0300

    added blank achievment_activity

--- app/src/main/java/activities/CitiesActivity.java
+++ app/src/main/java/activities/CitiesActivity.java
@@ -1,13 +1,18 @@
 package activities;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.mikhail.cubike.R;
 import com.example.mikhail.cubike.adapters.CityAdapter;
@@ -24,6 +29,10 @@ public class CitiesActivity extends ActionBarActivity {
 
     private RecyclerView recyclerView_;
     private CityAdapter cityAdapter_;
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private NavigationView mainNavigationMenu_;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,37 @@ public class CitiesActivity extends ActionBarActivity {
         recyclerView_ = (RecyclerView)findViewById(R.id.cities_recycler_view);
         recyclerView_ .setLayoutManager(new LinearLayoutManager(this));
         recyclerView_.setItemAnimator(new DefaultItemAnimator());
+
+       mainNavigationMenu_ = (NavigationView) findViewById(R.id.main_drawer);
+
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_activity_drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        mainNavigationMenu_.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    // Show all movies
+                    case R.id.achieves_menu_btn:
+
+                        Toast.makeText(CitiesActivity.this,"Pressed",Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(CitiesActivity.this,AchievmentActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+
+        setSupportActionBar(mToolbar);
 
        DatabaseHelper dbHelper = new DatabaseHelper(this);
         dbHelper.openDataBase();
